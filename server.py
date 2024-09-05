1d91ee88f37055608cf5d15d2f659e63e495f74d
Dave <wangebb@bc.edu>
2024-09-04 20:00:12 -0400

    new

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # set Middleware to allow all communication
 app.add_middleware(
@@ -17,7 +21,11 @@ class Message(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    try:
+        return FileResponse('static/index.html')
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="File not found")
+
 
 @app.post("/message")
 async def reply_message(message: Message):
